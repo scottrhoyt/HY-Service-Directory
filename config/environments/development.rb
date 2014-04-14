f43431dad1f117c3477f6cfea2d6a42f9a78a157
Scott Hoyt <scottrhoyt@gmail.com>
2014-04-14 01:12:36 -0700

    Updated settings to work with mail catcher.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,5 +38,5 @@ Rails.application.configure do
   # This is per Devise documentation
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 9000}
 end
