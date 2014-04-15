131361487f1cc36ef1f2a9e6d32f48749300ff28
Scott Hoyt <scottrhoyt@gmail.com>
2014-04-14 17:38:20 -0700

    Add service provider validations.

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -1,3 +1,4 @@
 class ServiceProvider < ActiveRecord::Base
   has_and_belongs_to_many :service_types
+  validates_presence_of :name, :address, :city, :state, :zipcode, :latitude, :longitude, :phone
 end
